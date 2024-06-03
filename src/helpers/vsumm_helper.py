7d5c92a25d6e721ecb0dd6bf8a9a8d53342a5367
Trung Trực Nguyễn <trungtrucnguyen@192.168.0.101>
2024-06-03 14:50:46 +0700

    fix

--- src/helpers/vsumm_helper.py
+++ src/helpers/vsumm_helper.py
@@ -45,7 +45,7 @@ def knapsack(values: Iterable[int],
     knapsack_solver.init(values, [weights], [capacity])
     knapsack_solver.solve()
     packed_items = [x for x in range(0, len(weights))
-                    if knapsack_solver.bestSolutionContains(x)]
+                    if knapsack_solver.best_solution_contains(x)]
 
     return packed_items
 
